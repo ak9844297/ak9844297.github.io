c8232f25b12e6170cece8e574133cee2f4b7eaa8
=johnathanhung <=johnathanhung@iii.org.tw>
2023-04-28 16:58:11 +0800

    [fix] 微調

--- projects/projectNav.js
+++ projects/projectNav.js
@@ -1,10 +1,12 @@
 header ='    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top" id="sideNav">\
         <a class="navbar-brand js-scroll-trigger" href="#page-top">\
+            <span class="d-block d-lg-none">Jonathan\'s Projects</span>\
+        </a>\
+        <a class="navbar-brand js-scroll-trigger" href="PJ_index.html">\
             <span class="navbar-brand d-none d-lg-block">\
                 <h4 style="color: aliceblue;">Jonathan Hung</h4>\
                 <img class="img-fluid img-profile rounded-circle mx-auto mb-2" style="height:auto" src="../img/jonathan.jpg" alt="">\
             </span>\
-            <span class="d-block d-lg-none">Websec_project</span>\
         </a>\
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
             <span class="navbar-toggler-icon"></span>\
@@ -16,16 +18,15 @@ header ='    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-
                 </li>\
                 <li class="nav-item dropdown">\
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">\
-                      Dropdown link\
+                        專案列表\
                     </a>\
                     <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">\
-                      <li><a class="dropdown-item" href="#">Action</a></li>\
-                      <li><a class="dropdown-item" href="#">Another action</a></li>\
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
-                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
+                        <a class="dropdown-item" href="project_websec.html">NTUST Web sercurity</a>\
+                        <a class="dropdown-item" href="project_AI_final.html">NTUST AI Image classification</a>\
+                        <a class="dropdown-item" href="project_software.html">NTUST software engineering</a>\
+                        <a class="dropdown-item" href="project_graduate.html">NTUST 5G-ORAN Xapp Sandbox</a>\
+                        <a class="dropdown-item" href="project_edetector.html">iForensics E-detector</a>\
+                        <a class="dropdown-item" href="project_III.html">III Ngap fuzzing tool & MITM tool</a>\
                     </ul>\
                 </li>\
             </ul>\
@@ -38,11 +39,11 @@ header ='    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-
                     <div class="collapse" id="collapseExample">\
                       <div class="card card-body">\
                         <a class="dropdown-item" href="project_websec.html">NTUST Web sercurity</a>\
+                        <a class="dropdown-item" href="project_AI_final.html">NTUST AI Image classification</a>\
                         <a class="dropdown-item" href="project_software.html">NTUST software engineering</a>\
                         <a class="dropdown-item" href="project_graduate.html">NTUST 5G-ORAN Xapp Sandbox</a>\
                         <a class="dropdown-item" href="project_edetector.html">iForensics E-detector</a>\
                         <a class="dropdown-item" href="project_III.html">III Ngap fuzzing tool & MITM tool</a>\
-                        <a class="dropdown-item" href="project_AI_final.html">AI Image classification</a>\
                       </div>\
                     </div>\
                 </li>   \
@@ -51,5 +52,6 @@ header ='    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-
                 </li>\
             </ul>\
         </div>\
+    </nav>\
 '
 document.write(header);
