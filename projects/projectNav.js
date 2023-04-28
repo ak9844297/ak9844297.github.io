563bc362e637b5fe292aa2659054038b02d2ca9c
=johnathanhung <=johnathanhung@iii.org.tw>
2023-04-28 14:03:18 +0800

    add_projects template

---
+++ projects/projectNav.js
@@ -0,0 +1,53 @@
+header ='    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary fixed-top" id="sideNav">\
+        <a class="navbar-brand js-scroll-trigger" href="#page-top">\
+            <span class="navbar-brand d-none d-lg-block">\
+                <h4 style="color: aliceblue;">Jonathan Hung</h4>\
+                <img class="img-fluid img-profile rounded-circle mx-auto mb-2" style="height:auto" src="../img/jonathan.jpg" alt="">\
+            </span>\
+            <span class="d-block d-lg-none">Websec_project</span>\
+        </a>\
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">\
+            <span class="navbar-toggler-icon"></span>\
+        </button>\
+        <div class="collapse navbar-collapse" id="navbarResponsive">\
+            <ul class="d-block d-lg-none navbar-nav">\
+                <li class="nav-item">\
+                    <a class="nav-link js-scroll-trigger" href="../index.html">首頁</a>\
+                </li>\
+                <li class="nav-item dropdown">\
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">\
+                      Dropdown link\
+                    </a>\
+                    <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">\
+                      <li><a class="dropdown-item" href="#">Action</a></li>\
+                      <li><a class="dropdown-item" href="#">Another action</a></li>\
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
+                      <li><a class="dropdown-item" href="#">Something else here</a></li>\
+                    </ul>\
+                </li>\
+            </ul>\
+            <ul class="d-lg-block d-none navbar-nav">\
+                <li class="nav-item">\
+                    <button class="btn "style="color: white;" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">\
+                        專案列表\
+                        <span class="fas fa-angle-down"></span>\
+                      </button>\
+                    <div class="collapse" id="collapseExample">\
+                      <div class="card card-body">\
+                        <a class="dropdown-item" href="project_websec.html">2022 Fall NTUST Web sercurity</a>\
+                        <a class="dropdown-item" href="project_graduate.html">NTUST Graduate project</a>\
+                        <a class="dropdown-item" href="project_edetector.html">iForensics E-detector</a>\
+                        <a class="dropdown-item" href="project_III.html">III Ngap fuzzing tool & MITM tool</a>\
+                      </div>\
+                    </div>\
+                </li>   \
+                <li class="nav-item">\
+                    <a class="nav-link js-scroll-trigger" href="../index.html">首頁</a>\
+                </li>\
+            </ul>\
+        </div>\
+'
+document.write(header);
